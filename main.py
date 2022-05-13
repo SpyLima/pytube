45aa7020efe311ce7e7382be3ff0cc5b535b49c0
spylima <spy.lima44@gmail.com>
2022-05-12 22:11:30 -0300

    fix issues

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ th_out = requests.get(th, stream=True)
 
 header = """
 \t • TITLE = {}
-\t • VIEWS = {}
-\t • AUTHOR ={}
+\t • AUTHOR = {} |•| VIEWS = {}
 \t • PUBLISH IN = {}
 \t • DESCRIPTION = \n \n {} \n
 """
 
-print(header.format(yt.title, yt.views, yt.author, yt.publish_date, yt.description))
+print(header.format(yt.title, yt.author, yt.views, yt.publish_date, yt.description))
 
 if th_out.status_code == 200:
     subprocess.run(["/usr/bin/kitty", "icat"], input=th_out.content)
 else:
-    print('\tcannot print the url')
+    print('\tcannot print the thumbnail')
